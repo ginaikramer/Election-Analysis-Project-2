2e36d4148ff47f886eadd47ee12ba8e7c8f12c7d
ginaikramer <91392131+ginaikramer@users.noreply.github.com>
2021-11-13 15:35:16 -0500

    Adding the election analysis

--- PyPoll.py
+++ PyPoll.py
@@ -45,29 +45,46 @@ with open(file_to_open) as election_data:
         #Add a vote to the candidate's count
         candidate_votes[candidate_name] += 1
 
-#for each of the candidates listed in candidate_votes          
-for candidate_name in candidate_votes:
-    #set votes to the total votes for that candidate
-    votes=candidate_votes[candidate_name]
-    #calculate vote percentage by dividing each candidate's vote by total votes. format it to 2 decimal places
-    vote_percentage = '{:.2f}'.format(float(votes) / float(total_votes) *100)
-    
-    print(f"{candidate_name}: {float(vote_percentage):.1f}% ({votes:,})\n")
+#initialize the text file you plan to save results to
+with open(file_to_save,"w") as txt_file:
+    #Print the final vote count to the terminal
+    election_results=(
+        f"\nElection Results\n"
+        f"-----------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-----------------------------\n")
+    print(election_results, end="")
+    #Save the final vote count to the text file
+    txt_file.write(election_results)
 
-    #Determine winning vote count and candidate
-    if (votes > winning_count) and (float(vote_percentage) > float(winning_percentage)):
-        #If this is true, set the winning_count to votes and winning_percentage to vote_percentage
-        winning_count=votes
-        winning_percentage=vote_percentage
-        #Set the winning candidate equal to the candidate's name
-        winning_candidate=candidate_name
+    #for each of the candidates listed in candidate_votes          
+    for candidate_name in candidate_votes:
+        #set votes to the total votes for that candidate
+        votes=candidate_votes[candidate_name]
+        #calculate vote percentage by dividing each candidate's vote by total votes. format it to 2 decimal places
+        vote_percentage = '{:.2f}'.format(float(votes) / float(total_votes) *100)
+        
+        candidate_results=((f"{candidate_name}: {float(vote_percentage):.1f}% ({votes:,})\n"))
+        #Print each candidate, their vote count and vote percentage to the terminal
+        print(candidate_results)
+        #Save the candidate results to the election analysis text file
+        txt_file.write(candidate_results)
 
-winning_candidate_summary=(
-    f"------------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {float(winning_percentage):.1f}%\n"
-    f"-------------------------\n")
-print(winning_candidate_summary)
+        #Determine winning vote count and candidate
+        if (votes > winning_count) and (float(vote_percentage) > float(winning_percentage)):
+            #If this is true, set the winning_count to votes and winning_percentage to vote_percentage
+            winning_count=votes
+            winning_percentage=vote_percentage
+            #Set the winning candidate equal to the candidate's name
+            winning_candidate=candidate_name
 
+    #Print the results to the terminal
+    winning_candidate_summary=(
+        f"-----------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {float(winning_percentage):.1f}%\n"
+        f"-----------------------------\n")
+    #print(winning_candidate_summary)
+    txt_file.write(winning_candidate_summary)
 
